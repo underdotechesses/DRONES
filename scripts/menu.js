65a3223e9ff31ae8f67501b17c6429189abf504e
Mykola Marchenko <nickolay91509031@gmail.com>
2023-08-30 22:21:58 +0200

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,25 +1,24 @@
 export default class Menu {
     constructor() {
-        this.menu = document.querySelector(".close-menu");
-        this.menuItems = document.querySelector(".menu-container-hidden");
+        this.menu = document.querySelector(".btn-menu");
+        this.menuItems = document.querySelector(".menu-ul");
+
         this.render();
     }
 
     addMenuItem() {
-        this.menuItems.className = "menu-container";
-        this.menu.classList.add("menu");
+        this.menuItems.removeAttribute("hiddden");
     }
 
     closeMenuItem() {
-        this.menuItems.className = "menu-container-hidden";
-        this.menu.classList.remove("menu");
+        this.menuItems.setAttribute("hiddden");
     }
 
     render() {
-        if (this.menu.classList.contains("menu")) {
-            this.menu.addEventListener("click", this.closeMenuItem.bind(this));
-        } else {
+        if (this.menuItems.hasAttribute("hidden")) {
             this.menu.addEventListener("click", this.addMenuItem.bind(this));
+        } else {
+            this.menu.addEventListener("click", this.closeMenuItem.bind(this));
         }
     }
 }
