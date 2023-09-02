de0a5c9ee29c0e845497444ed6b2d8b08011afbf
Mykola Marchenko <nickolay91509031@gmail.com>
2023-09-02 20:35:58 +0200

    update

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,30 @@
 export default class Login {
     constructor() {
+        this.btnLoginOk = document.querySelector(".button-ok");
         this.btnLoginCross = document.querySelector(".login-cross");
         this.btnLogin = document.querySelector(".login");
         this.loginPlace = document.querySelector(".login-ul");
         this.loginText = document.querySelector(".login-li");
         this.passwordText = document.querySelector(".password");
 
+        this.loginValue = this.loginText.value;
+        this.passwordValue = this.passwordText.value;
+
         this.render();
+    }
+
+    toggleLogin() {
+        this.closeSingInPlace();
+        this.btnLogin.remove();
+
+        this.mainUl = document.querySelector(".second-ul");
+        this.loginCircle = document.createElement("li");
+        this.loginSpan = document.createElement("span");
+
+        this.loginCircle.setAttribute("class", "login-circle");
+
+        this.mainUl.prepend(this.loginCircle);
+        this.loginCircle.appendChild(this.loginSpan);
     }
 
     addSingInPlace() {
@@ -31,5 +49,6 @@ export default class Login {
             "click",
             this.closeSingInPlace.bind(this)
         );
+        this.btnLoginOk.addEventListener("click", this.toggleLogin.bind(this));
     }
 }
